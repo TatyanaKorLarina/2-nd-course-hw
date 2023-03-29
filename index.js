e21b814e9ed954fd9a952d4bcf5819b6208c3b99
TaTia <tatyanakoryachkina@yandex.ru>
2023-03-29 18:15:39 +0300

    changed the file index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@
 //let a = "20";
 //alert (a);
 
-let year = "год выпуска первого iPhone";
+/*let year = "год выпуска первого iPhone";
 alert (year);
 
 let creator = "имя создателя языка JavaScript";
-alert (creator);
+alert (creator);*/
 
 /*let b = 10;
 let c = 2;
@@ -19,7 +19,7 @@ alert (b/c);
 let result = c ** 5;
 alert (result);*/
 
-let a = 9;
+/*let a = 9;
 let b = 2;
 alert (a % b);
 
@@ -52,7 +52,99 @@ console.log(user[info]);
 
 let userName = prompt('Как вас зовут?');
 let greeting = `Привет, ${userName}!`;
-alert(greeting);
+alert(greeting);*/
+
+let password = 'пароль';
+let input = prompt ('Введите пароль');
+if (input === password) {
+    alert ('Пароль введен верно');
+} else {
+    alert ('Пароль введен неправильно');
+}
+
+let c = prompt ('Введите любое число');
+if (c>0 && c<10) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+let d = prompt ('Введите первое число');
+let e = prompt ('Введите второе число');
+if (e>100 || d>100) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+let a = 2;
+let b = 3;
+alert (a+b);
+
+let monthNumber = Number (prompt ('Введите номер месяца'));
+switch (monthNumber) {
+    case 12:
+    case 1:
+    case 2:
+        console.log('Принадлежит к сезону "Зима"')
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log('Принадлежит к сезону "Весна"')
+        break;
+    case 6:
+    case 7:
+    case 8:
+        console.log('Принадлежит к сезону "Лето"')
+        break;
+    case 9:
+    case 10:
+    case 11:
+        console.log('Принадлежит к сезону "Осень"')
+        break;
+    default:
+        console.log('Такого месяца не существует');
+        break;
+}
+
+let num = Number (prompt ('Пожалуйста, введите любое число'));
+num = Math.abs (num);
+if (num%2 ===1) {
+    alert ('Число нечетное');
+} else if (num%2 ===0) {
+    alert ('Число четное');
+} else {
+    alert ('Это не число');
+}
+
+let clientOS = Number (prompt ('Укажите, пожалуйста, 0, если вы используете iOS, и 1, если - Android'));
+if (clientOS === 0) {
+    console.log ('Установите версию приложения для iOS по ссылке'); 
+} else if (clientOS === 1) {
+    console.log ('Установите версию приложения для Android по ссылке');
+} else {
+    console.log ('Мы не работаем с данной ОС');
+} 
+    
+let clientDeviceYear = Number (prompt ('Пожалуйста, укажите год выпуска вашего устройства'));
+if (clientOS === 0 && clientDeviceYear >=2015) {
+    console.log ('Установите версию приложения для iOS по ссылке');
+} else if (clientOS === 0 && clientDeviceYear < 2015) {
+    console.log('Установите облегченную версию приложения для iOS по ссылке'); 
+} else if (clientOS === 1 && clientDeviceYear >=2015) {
+    console.log ('Установите версию приложения для Android по ссылке');
+} else if (clientOS === 1 && clientDeviceYear < 2015) {
+    console.log('Установите облегченную версию приложения для Android по ссылке'); 
+} else {
+    console.log('Мы не работаем с данной ОС');
+}
+
+
+
+    
+
+
 
 
 
