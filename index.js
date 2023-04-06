3f7a8f47041b365b0e8fc60cebdedd69cae72e38
TaTia <tatyanakoryachkina@yandex.ru>
2023-04-07 01:19:38 +0300

    changed index.js and created a new section in index.html

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ if (clientOS === 0 && clientDeviceYear >=2015) {
     console.log('Мы не работаем с данной ОС');
 }*/
 
-let i = 1;
+/*let i = 1;
 let l = 2;
 while (i<=l) {
     console.log('Привет');
@@ -187,9 +187,160 @@ for (let dayNumber = dayNumberFri; dayNumber<=31; dayNumber++) {
         alert(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
         continue;
     }
+}*/
+
+function min(a, b) {
+    if (a <= b) {
+        return a;
+    } else {
+        return b;
+    }
 }
 
+let result = min(8,4);
+alert (result); 
+
+let result1 = min(6,6);
+alert (result1);
+
+function even(n1) {
+    if ( n1 % 2 == 0) {
+        return 'Число четное';
+    } else {
+        return 'Число нечетное';
+    }
+}
+
+let result3 = even(22);
+console.log(result3);
+
+let result4 = even(37);
+console.log(result4);
+
+function square(num) {
+    console.log(num ** 2);   
+}
+square(3);
+square(21);
+
+function squareReturn(num1) {
+    return(num1 ** 2);   
+}
     
+let result5 = squareReturn(22);
+console.log(result5);
+
+let result6 = squareReturn(37);
+console.log(result6);
+
+"use strict";
+let age = prompt("Сколько Вам лет?", 13);
+if (age < 0) {
+    function printMessage() {
+      alert("Вы ввели неправильное значение");  
+    }
+} else if (age < 13) {
+    function printMessage() {
+       alert("Привет, друг"); 
+    }
+} else {
+    function printMessage() {
+        alert("Добро пожаловать!");
+    }
+}
+
+ printMessage();
+
+ //let a = Number;
+ //let b = Number;
+
+ function correctNumber(a, b) {
+    if (!isNaN(a || b)) {
+        return a * b;
+    } else {
+        return 'Одно или оба значения не являются числом';
+    }
+ }
+
+ let result7 = correctNumber(2, 3);
+ console.log(result7);
+
+ let result8 = correctNumber(-2, 13);
+ console.log(result8);
+
+ let result9 = correctNumber('hi', 3);
+ console.log(result9);
+
+let n = prompt('Пожалуйста, введите любое число');
+function cube(n) {
+    if (!isNaN(n)) {
+        let result10 = n ** 3;
+        return `${n} в кубе равняется ${result10}`;
+    } else {
+        return 'Переданный параметр не является числом';
+    }
+}
+
+let result11 = cube(0);
+console.log(result11);
+
+let result12 = cube(1);
+console.log(result12);
+
+let result13 = cube(2);
+console.log(result13);
+
+let result14 = cube(3);
+console.log(result14);
+
+let result15 = cube(4);
+console.log(result15);
+
+let result16 = cube(5);
+console.log(result16);
+
+let result17 = cube(6);
+console.log(result17);
+
+let result18 = cube(7);
+console.log(result18);
+
+let result19 = cube(8);
+console.log(result19);
+
+let result20 = cube(9);
+console.log(result20);
+
+let result21 = cube(10);
+console.log(result21);
+
+const π = 3.14;
+
+function getCircleArea() {
+    return π * this.radius**2;    
+}
+function getCirclePerimeter() {
+    return 2 * π * this.radius;   
+}
+
+const circle1 = {
+    radius: 10,
+    
+    getArea: getCircleArea,
+    getPerimeter: getCirclePerimeter,
+};
+
+const circle2 = {
+    radius: 33,
+    
+    getArea: getCircleArea,
+    getPerimeter: getCirclePerimeter,
+};
+
+console.log(circle1.getArea());
+console.log(circle1.getPerimeter());
+console.log(circle2.getArea());
+console.log(circle2.getPerimeter());
 
 
 
