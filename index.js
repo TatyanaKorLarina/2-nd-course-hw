b2aa91cef047850488e14cdee6ee824b5dd42b7a
TaTia <tatyanakoryachkina@yandex.ru>
2023-04-09 18:04:48 +0300

    corrected the first task

--- index.js
+++ index.js
@@ -354,11 +354,12 @@ function getSeason() {
     } 
 }*/
 
-/*const numbers = [1, 5, 4, 10, 0, 3];
+const numbers = [1, 5, 4, 10, 0, 3];
 for (let i = 0; i < numbers.length; i++) {
-    if (numbers [i] == 10) break;
     console.log(numbers [i]);
-}*/
+    if (numbers [i] == 10) break;
+    
+}
 
 const numbs = [1, 5, 4, 10, 0, 3];
 numbs.forEach ((el, index) => {
