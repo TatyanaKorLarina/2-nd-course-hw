f9f0da53f629512e13770efce1dd5e8eb2bc33a8
TaTia <tatyanakoryachkina@yandex.ru>
2023-04-15 23:44:31 +0300

    did the 2nd task of the 8th hw

--- index.js
+++ index.js
@@ -611,3 +611,20 @@ function getResult(arr, callback) {
 getResult ([3, 4, 1, 9], mult); //108
 getResult ([3, 4, 1, 9], sum); //17
 
+function compareUser(a, b) {
+    if (a.age > b.age) return 1;
+    if (a.age < b.age) return -1;
+    return 0;        
+};   
+
+
+const users = [
+	{name: 'Jon', age: 22},
+	{name: 'Richard', age: 18},
+	{name: 'Anton', age: 32},
+	{name: 'Lida', age: 23},
+	{name: 'Bob', age: 44}
+];
+users.sort(compareUser);
+console.log(users);
+
